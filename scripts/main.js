4638c5e84c0bac437a9b8de0adfe27817b879d60
A0l1i1c3e4 <vas120704@gmail.com>
2025-06-13 19:03:26 +0700

    Defoul pages

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const loginButton = document.getElementById('in');
+const profileButton = document.getElementById('profileButton');
+const logoutButton = document.getElementById('logoutButton');
+const userMenu = document.getElementById('userMenu');
+let userMenuListenerAttached = false;
+
+function activate(email){
+  if (!userMenuListenerAttached) {
+    loginButton.addEventListener('click', () => {
+    userMenu.style.display = userMenu.style.display === 'block' ? 'none' : 'block';
+    });
+    loginButton.textContent = email + ' ▾';
+    profileButton.style.display = 'inline-block';
+    logoutButton.style.display = 'inline-block';
+
+    profileButton.addEventListener('click', () => {
+      window.location.href = '../pages/profile.html'
+    });
+
+    logoutButton.addEventListener('click', () => {
+      localStorage.removeItem('token');
+      window.location.href = '../pages/login.html'
+    });
+    userMenuListenerAttached = true;
+  }
+}
+window.addEventListener('load', () => {
+    const authToken = localStorage.getItem('token');
+    if (authToken) {
+      console.log('Токен получен из localStorage:', localStorage.getItem('token'));
+      email=localStorage.getItem('email')
+      document.getElementById('in').textContent=email;
+      activate(email);
+    } else {
+      console.log('Токен не найден в localStorage.');
+      window.location.href = '../pages/login.html'
+    }
+});
